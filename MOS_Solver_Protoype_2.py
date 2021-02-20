681bb1203bccde3c8860f4866d3de87f2e03848b
tansecoc <tansecoc@oregonstate.edu>
2021-02-20 12:07:24 -0800

    This push includes continued updates to prototype 2.

--- MOS_Solver_Protoype_2.py
+++ MOS_Solver_Protoype_2.py
@@ -2,10 +2,11 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from collections import Counter
 import random
+import copy
 
-# status = box class status
-# key = box ID number
-# element = web-click element
+# box_status = box class status
+# box_ID = box ID number
+# box_element = web-click element
 
 # opens minesweeper website
 PATH = "/Users/casimerotanseco/chromedriver/chromedriver"
@@ -13,25 +14,32 @@ driver = webdriver.Chrome(PATH)
 driver.get("https://minesweeperonline.com/#beginner")
 
 # creates dictionary of box statuses, ID, and web-click elements for a 9x9 minesweeper grid
-dict_of_box_keys_elements = {}
+dict_of_boxes = {}
 for i in range(1, 10):
     for j in range(1, 10):
-        key_dict_variable = "x_" + str(i) + "_" + str(j)
-        key_ID = str(i) + "_" + str(j)
-        element = driver.find_element_by_id(key_ID)
-        dict_of_box_keys_elements[key_dict_variable] = ["square blank", element]
+        box_ID_variable = "x_" + str(i) + "_" + str(j)
+        box_ID_key = str(i) + "_" + str(j)
+        box_element = driver.find_element_by_id(box_ID_key)
+        dict_of_boxes[box_ID_variable] = ["square blank", box_element]
 
-# sets smiley face value
+# create safe dictionary for guessing
+safe_dict_of_boxes = {}
+for i in range(1, 10):
+    for j in range(1, 10):
+        safe_box_ID_variable = "x_" + str(i) + "_" + str(j)
+        safe_dict_of_boxes[safe_box_ID_variable] = 0
+
+# set initial face value
 # facesmile = game not started or game in progress
 # faceooh = mouseclick in progress
 # facewin = game has been won
-# facedead = gaeme over; hit bomb
-face = driver.find_element_by_id("face")
-val_face = face.get_attribute("class")
+# facedead = game over; hit bomb
+face_element = driver.find_element_by_id("face")
+face_status_value = face_element.get_attribute("class")
 
 # game play counter and # of times to run program
 counter = 0
-games_to_play = 1000000
+games_to_play = 10
 
 # mineseweper solver algorithm
 for i in range(1, games_to_play + 1):
@@ -39,10 +47,54 @@ for i in range(1, games_to_play + 1):
     print("Start Game:", i)
     counter += 1
 
-    # click 4 corners for first move
-    dict_of_box_keys_elements["x_1_1"][1].click()
-    dict_of_box_keys_elements["x_1_9"][1].click()
-    dict_of_box_keys_elements["x_9_1"][1].click()
-    dict_of_box_keys_elements["x_9_9"][1].click()
+    # click 4 corners as first four moves then delete boxes from safe dictionary
+    four_box_counter = 1
+    if four_box_counter < 1:
+        dict_of_boxes["x_1_1"][1].click()
+        del safe_dict_of_boxes["x_1_1"]
+        dict_of_boxes["x_1_9"][1].click()
+        del safe_dict_of_boxes["x_1_9"]
+        dict_of_boxes["x_9_1"][1].click()
+        del safe_dict_of_boxes["x_9_1"]
+        dict_of_boxes["x_9_9"][1].click()
+        del safe_dict_of_boxes["x_9_9"]
+        four_box_counter += 1
 
+    # check face value
+    face_element = driver.find_element_by_id("face")
+    face_status_value = face_element.get_attribute("class")
 
+    # pause program if won
+    if face_status_value == "facewin":
+        input("YOU HAVE WON! PROGRAM PAUSED!!!")
+
+    # restart game if face == dead
+    if face_status_value == "facedead":
+        face_element.click()
+
+    # begin click guesses
+    else:
+        # if face is != 'facedead' then continue guesses
+        while face_status_value != "facedead" or face_status_value != "facewin":
+
+            # click random box
+            current_box_key = random.choice(list(dict_of_boxes.keys()))
+            current_box_element = dict_of_boxes[current_box_key][1]
+            current_box_status = current_box_element.get_attribute("class")
+
+            # if current_box_status ==
+
+            print("Current box status:", current_box_status)
+            current_box_element.click()
+
+            current_box_status = current_box_element.get_attribute("class")
+            print("Current box status:", current_box_status)
+
+            # check face value after click
+            face_element = driver.find_element_by_id("face")
+            face_status_value = face_element.get_attribute("class")
+            print("Face value status:", face_status_value)
+
+            # pause program if won
+            if face_status_value == "facewin":
+                input("YOU HAVE WON! PROGRAM PAUSED!!!")
