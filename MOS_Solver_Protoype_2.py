8d4ccc1e83d535155308cb6a1b2b26313b537dcb
tansecoc <tansecoc@oregonstate.edu>
2021-02-20 12:57:24 -0800

    Four-corners guess implemented.

--- MOS_Solver_Protoype_2.py
+++ MOS_Solver_Protoype_2.py
@@ -47,18 +47,38 @@ for i in range(1, games_to_play + 1):
     print("Start Game:", i)
     counter += 1
 
-    # click 4 corners as first four moves then delete boxes from safe dictionary
-    four_box_counter = 1
-    if four_box_counter < 1:
-        dict_of_boxes["x_1_1"][1].click()
-        del safe_dict_of_boxes["x_1_1"]
-        dict_of_boxes["x_1_9"][1].click()
-        del safe_dict_of_boxes["x_1_9"]
-        dict_of_boxes["x_9_1"][1].click()
-        del safe_dict_of_boxes["x_9_1"]
-        dict_of_boxes["x_9_9"][1].click()
-        del safe_dict_of_boxes["x_9_9"]
-        four_box_counter += 1
+    # First Four Moves: click 4 corners, obtain box statuses, delete boxes from safe dictionary
+    try:
+        current_box_key = "x_1_1" # sets current box key to top-left corner
+        dict_of_boxes[current_box_key][1].click()  # clicks corner
+        current_box_element = dict_of_boxes[current_box_key][1] # sets current box element to corner
+        current_box_status = current_box_element.get_attribute("class") # obtains box status
+        dict_of_boxes[current_box_key][0] = current_box_status # updates box status in box dictionary
+        del safe_dict_of_boxes[current_box_key ]# deletes safe box key from safe dictionary
+
+        current_box_key = "x_1_9" # sets current box key to top-right corner
+        dict_of_boxes[current_box_key][1].click()  # clicks corner
+        current_box_element = dict_of_boxes[current_box_key][1] # sets current box element to corner
+        current_box_status = current_box_element.get_attribute("class") # obtains box status
+        dict_of_boxes[current_box_key][0] = current_box_status # updates box status in box dictionary
+        del safe_dict_of_boxes[current_box_key ]# deletes safe box key from safe dictionary
+
+        current_box_key = "x_9_1" # sets current box key to bottom-left corner
+        dict_of_boxes[current_box_key][1].click()  # clicks corner
+        current_box_element = dict_of_boxes[current_box_key][1] # sets current box element to corner
+        current_box_status = current_box_element.get_attribute("class") # obtains box status
+        dict_of_boxes[current_box_key][0] = current_box_status # updates box status in box dictionary
+        del safe_dict_of_boxes[current_box_key ]# deletes safe box key from safe dictionary
+
+        current_box_key = "x_9_9" # sets current box key to bottom-right corner
+        dict_of_boxes[current_box_key][1].click()  # clicks corner
+        current_box_element = dict_of_boxes[current_box_key][1] # sets current box element to corner
+        current_box_status = current_box_element.get_attribute("class") # obtains box status
+        dict_of_boxes[current_box_key][0] = current_box_status # updates box status in box dictionary
+        del safe_dict_of_boxes[current_box_key ]# deletes safe box key from safe dictionary
+
+    except:
+        continue
 
     # check face value
     face_element = driver.find_element_by_id("face")
@@ -69,32 +89,51 @@ for i in range(1, games_to_play + 1):
         input("YOU HAVE WON! PROGRAM PAUSED!!!")
 
     # restart game if face == dead
-    if face_status_value == "facedead":
+    elif face_status_value == "facedead":
         face_element.click()
 
     # begin click guesses
     else:
-        # if face is != 'facedead' then continue guesses
+
+        # if game is not dead or won then continue guessing loop
         while face_status_value != "facedead" or face_status_value != "facewin":
 
-            # click random box
-            current_box_key = random.choice(list(dict_of_boxes.keys()))
-            current_box_element = dict_of_boxes[current_box_key][1]
-            current_box_status = current_box_element.get_attribute("class")
+            # click random box from safe dictionary
+            current_box_key = random.choice(list(safe_dict_of_boxes.keys())) # sets random current box
+            current_box_element = dict_of_boxes[current_box_key][1] # sets current box web-click element
+            current_box_element.click() # clicks current box
 
-            # if current_box_status ==
+            # obtain current box status, deletes current box from safe dictionary
+            current_box_status = current_box_element.get_attribute("class") # obtains box status
+            dict_of_boxes[current_box_key][0] = current_box_status  # updates box status in box dictionary
+            del safe_dict_of_boxes[current_box_key]  # deletes safe box key from safe dictionary
+            print("Clicked box:", current_box_key, ". Status:", current_box_status)
 
-            print("Current box status:", current_box_status)
+            print("Dict of boxes:", dict_of_boxes)
+            print("Safe dict of boxes:", safe_dict_of_boxes)
+            input("stop")
+
+
+
+
+
+            # if current_box_status == "square open0":
+
             current_box_element.click()
-
             current_box_status = current_box_element.get_attribute("class")
             print("Current box status:", current_box_status)
 
-            # check face value after click
-            face_element = driver.find_element_by_id("face")
-            face_status_value = face_element.get_attribute("class")
-            print("Face value status:", face_status_value)
+            if current_box_status == "square open0":
+
+            # END OF WHILE LOOP - check face value after click
+                face_element = driver.find_element_by_id("face")
+                face_status_value = face_element.get_attribute("class")
 
             # pause program if won
             if face_status_value == "facewin":
+                print("Face value status:", face_status_value)
                 input("YOU HAVE WON! PROGRAM PAUSED!!!")
+
+            # restart guessing while loop
+            if face_status_value == "facedead":
+                print("Face value status:", face_status_value + ". Restarting game.")
